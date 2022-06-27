c030c0b9ced7312ccbf33a699aa1fc1a65844ad9
rahul <rahul.valasai@gmail.com>
2022-06-27 15:33:20 +0530

    add-sub-mul-div.

---
+++ add-sub-mul-div.js
@@ -0,0 +1,34 @@
+//addition
+ let a = 10;
+
+ let b = 5;
+
+ let add = a + b;
+
+ console.log( "Addition is :" + add);
+
+//subtrtaction
+
+let c = 30,d = 20;
+
+let sub = a - b;
+
+console.log(sub);
+
+//multiplication
+
+ let e = 10,f = 5;
+
+ let mul = a * b;
+
+ console.log("multiplication is:",mul);
+
+// division
+
+let g = 10, h = 20;
+
+ let div = a / b;
+
+ console.log("division is:",div); 
+
+ 
